7d5f94cedee76dd9109bfdd2f3f0f0767d6670e5
alperduru <alper.duru@accelerize360.com>
2021-10-10 21:55:37 -0500

    Calculating the quality of life.

---
+++ qualityAdjustedLifeYear.cpp
@@ -0,0 +1,22 @@
+/* A program to calculate the quality of life.
+ * Link: https://open.kattis.com/problems/qaly
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    double total = 0;
+    cin >> n;
+
+    for (int a = 0; a < n; a++)
+    {
+        double q, y;
+        cin >> q >> y;
+        total += q * y;  
+    }
+    cout << total;
+    return 0;
+}
