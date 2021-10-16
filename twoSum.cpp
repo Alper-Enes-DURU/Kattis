77914f863c49731120405d322e0f5f7d9ed89c8f
alperduru <enesduru@gmail.com>
2021-10-15 23:35:48 -0500

    A program to sum two numbers.

---
+++ twoSum.cpp
@@ -0,0 +1,14 @@
+/* A program to calculate the two numbers.
+ * Link: https://open.kattis.com/problems/twosum
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
