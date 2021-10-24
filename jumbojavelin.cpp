dc533560bd12965c73924db5c6f0e6cdc69df593
alperduru <enesduru@gmail.com>
2021-10-23 23:49:11 -0500

    Calculate the length of the javelin.

---
+++ jumbojavelin.cpp
@@ -0,0 +1,24 @@
+/* A program to calculate the length of the javelin.
+ * Link: https://open.kattis.com/problems/jumbojavelin
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        sum += x;
+    }
+
+    cout << sum - (n - 1);
+
+    return 0;  
+}
