ad7aacd76be09f1e96faf2b636342b210ec9eb70
alperduru <enesduru@gmail.com>
2021-10-24 22:10:30 -0500

    Calculate the are of a triangle.

---
+++ triarea.cpp
@@ -0,0 +1,17 @@
+/* A program to calculate the area of a triangle.
+ * Link: https://open.kattis.com/problems/triarea
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cin >> x >> y;
+
+    cout << (x * y) / 2.0;
+
+    return 0;
+}
