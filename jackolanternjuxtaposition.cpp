d32c8a43f1c188d1d0f0a10c09014a5bccedf532
alperduru <enesduru@gmail.com>
2021-10-30 21:25:12 -0500

    A program to multiple a series of numbers.

---
+++ jackolanternjuxtaposition.cpp
@@ -0,0 +1,17 @@
+/* A program to multiple a series of numbers.
+ * Link: https://open.kattis.com/problems/jackolanternjuxtaposition
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+
+    cin >> x >> y >> z;
+
+    cout << x * y * z;
+    
+    return 0;
+}
