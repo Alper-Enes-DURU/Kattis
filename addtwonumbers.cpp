a4e17aa0b27e73843fab854fc63d5156356a3baa
alperduru <enesduru@gmail.com>
2021-11-14 21:51:32 -0600

    A program to sum two given numbers.

---
+++ addtwonumbers.cpp
@@ -0,0 +1,15 @@
+/* A program to sum two given numbers.
+ * Link: https://open.kattis.com/problems/addtwonumbers
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << x + y;
+    
+    return 0;
+}
